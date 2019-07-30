0711f37fe0a536daea3069da7247ecef461855d7
Nayeemul Islam Swad <drswad2013@gmail.com>
2019-07-30 15:13:00 +0600

    Add solution for problem `D - Stressful Training` Link: https://codeforces.com/contest/1132/problem/D Tags: binary search, greedy, *2300 Ref: https://codeforces.com/contest/1132/submission/57980501

--- codeforces/1132/D.cpp
+++ codeforces/1132/D.cpp
@@ -1,71 +1,67 @@
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
+
 #define debug(a) cerr << #a << ": " << a << endl
- 
+
 typedef long long ll;
 typedef pair<ll, ll> ii;
- 
+
 #define x first
 #define y second
- 
+
 const int N = 2e5 + 10;
 const ll MAX_X = 1e13;
- 
+
 int n, k;
 ll a[N], b[N];
-vector<ii> Ends[N];
- 
+
 bool check(ll x) {
-	for (int i = 0; i <= k; i++) Ends[i].clear();
+	auto cmp = [](const pair<ll, ii>& p1, const pair<ll, ii>& p2) {
+		return p1.x > p2.x;
+	};
+	priority_queue<pair<ll, ii>, vector<pair<ll, ii>>, decltype(cmp)> pq(cmp);
 
 	for (int i = 1; i <= n; i++) {
-		Ends[min(a[i] / b[i], (ll)k)].push_back({a[i] % b[i], b[i]});
+		pq.push({a[i] / b[i], {a[i] % b[i], b[i]}});
 	}
 
-	int c = 0;
- 
-	for (int d = 0; d <= k - 2; d++) {
-		c++;
-		
-		for (ii p : Ends[d]) {
-			ll rem = p.x;
-			ll _b = p.y;
-	 
-			while (rem < _b) {
-				if (c == 0) return false;
+	for (int d = 0; d <= k - 1; d++) {
+		pair<ll, ii> top = pq.top();
+		pq.pop();
+		ll day = top.x;
+		ll rem = top.y.x;
+		ll _b = top.y.y;
 
-				rem += x;
-				c--;
-			}
-	 
-			Ends[min(d + rem / _b, (ll)k)].push_back({rem % _b, _b});
-		}
+		if (day < d) return false;
+
+		rem = rem + x;
+
+		pq.push({day + rem / _b, {rem % _b, _b}});
 	}
- 
+
 	return true;
 }
- 
+
 int main() {
 	#ifdef LOCAL
 	freopen("in", "r", stdin);
 	freopen("out", "w", stdout);
 	#endif
- 
+
 	cin >> n >> k;
 	for (int i = 1; i <= n; i++) scanf("%lld", &a[i]);
 	for (int i = 1; i <= n; i++) scanf("%lld", &b[i]);
- 
+
 	ll L = 0, R = MAX_X;
 	while (L != R) {
 		ll mid = (L + R) / 2;
 		if (check(mid)) R = mid;
 		else L = mid + 1;
 	}
- 
+
 	if (check(L)) cout << L << endl;
 	else cout << -1 << endl;
- 
+
 	return 0;
 }
