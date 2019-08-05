7aff178e25e98d409f9d6481ff7d33f296072768
Nayeemul Islam Swad <drswad2013@gmail.com>
2019-08-05 20:05:00 +0600

    Add solution for problem `G - (Zero XOR Subset)-less` Link: https://codeforces.com/contest/1101/problem/G Tags: math, matrices, *2300 Ref: https://codeforces.com/contest/1101/submission/58343569

--- codeforces/1101/G.cpp
+++ codeforces/1101/G.cpp
@@ -2,49 +2,71 @@
  
 using namespace std;
  
+#define debug(a) cerr << #a << ": " << a << endl
+ 
+typedef long long ll;
+typedef pair<int, int> ii;
+ 
+#define x first
+#define y second
+ 
 const int N = 2e5 + 10, LOG_PREF = 31;
- 
+
 int n;
-int basis[LOG_PREF];
- 
-void insertVector(int mask) {
+int base[LOG_PREF];
+
+void tryGauss(int mask) {
 	for (int i = 0; i < LOG_PREF; i++) {
 		if ((mask & 1 << i) == 0) continue;
- 
-		if (!basis[i]) {
-			basis[i] = mask;
+
+		if (!base[i]) {
+			base[i] = mask;
 			return;
 		}
- 
-		mask ^= basis[i];
+
+		mask ^= base[i];
+	}
+
+	if (mask != 0) {
+		for (int i = 0; i < LOG_PREF; i++) {
+			if ((mask & 1 << i) > 0) {
+				base[i] = mask;
+				return;
+			}
+		}
 	}
 }
- 
+
 int main() {
+	#ifdef LOCAL
+	freopen("in", "r", stdin);
+	freopen("out", "w", stdout);
+	#endif
+
 	cin >> n;
- 
+
 	int pref = 0;
- 
+
 	for (int i = 1; i <= n; i++) {
 		int a;
 		scanf("%d", &a);
- 
+
 		pref ^= a;
-		insertVector(pref);
+		tryGauss(pref);
 	}
- 
+
 	if (pref == 0) {
 		cout << -1 << endl;
 		return 0;
 	}
- 
+
 	int ans = 0;
- 
+
 	for (int i = 0; i < LOG_PREF; i++) {
-		ans += (basis[i] > 0);
+		ans += (base[i] > 0);
 	}
- 
+
 	cout << ans << endl;
- 
+
 	return 0;
 }
