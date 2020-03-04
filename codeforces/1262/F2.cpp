426b64edd399ac9670936a36d6999dbb9c1ffce4
Nayeemul Islam Swad <drswad2013@gmail.com>
2020-03-04 18:46:00 +0600

    Add solution for problem `F2 - Wrong Answer on test 233 (Hard Version)` Link: https://codeforces.com/contest/1262/problem/F2 Tags: combinatorics, math, *2400 Ref: https://codeforces.com/contest/1262/submission/72413754

--- codeforces/1262/F2.cpp
+++ codeforces/1262/F2.cpp
@@ -11,18 +11,101 @@ typedef pair<int, int> pii;
 #include "debug.h"
 #endif
 
-const int N = int(2e5) + 10;
+const int G = 3;
 const int MOD = 998244353;
+const int N = (1 << 20) + 5;
+
+int rev[N], w[N], inv_n;
+
+int bigMod (int a, int e, int mod) {
+	if (e == -1) e = mod - 2;
+	int ret = 1;
+	while (e) {
+		if (e & 1) ret = (ll) ret * a % mod;
+		a = (ll) a * a % mod; e >>= 1;
+	}
+	return ret;
+}
+
+void prepare (int &n) {
+	int sz = abs(31 - __builtin_clz(n));
+	int r = bigMod(G, (MOD - 1) / n, MOD);
+	inv_n = bigMod(n, MOD - 2, MOD), w[0] = w[n] = 1;
+	for (int i = 1; i < n; ++i) w[i] = (ll) w[i - 1] * r % MOD;
+	for (int i = 1; i < n; ++i) rev[i] = (rev[i >> 1] >> 1) | ((i & 1) << (sz - 1));
+}
+
+void ntt (int *a, int n, int dir) {
+	for (int i = 1; i < n - 1; ++i) {
+		if (i < rev[i]) swap(a[i], a[rev[i]]);
+	}
+	for (int m = 2; m <= n; m <<= 1) {
+		for (int i = 0; i < n; i += m) {
+			for (int j = 0; j < (m >> 1); ++j) {
+				int &u = a[i + j], &v = a[i + j + (m >> 1)];
+				int t = (ll) v * w[dir ? n - n / m * j : n / m * j] % MOD;
+				v = u - t < 0 ? u - t + MOD : u - t;
+				u = u + t >= MOD ? u + t - MOD : u + t;
+			}
+		}
+	} if (dir) for (int i = 0; i < n; ++i) a[i] = (ll) a[i] * inv_n % MOD;
+}
+
+int f_a[N], f_b[N];
+
+vector <int> multiply (vector <int> a, vector <int> b) {
+	int sz = 1, n = a.size(), m = b.size();
+	while (sz < n + m - 1) sz <<= 1; prepare(sz);
+	for (int i = 0; i < sz; ++i) f_a[i] = i < n ? a[i] : 0;
+	for (int i = 0; i < sz; ++i) f_b[i] = i < m ? b[i] : 0;
+	ntt(f_a, sz, 0); ntt(f_b, sz, 0);
+	for (int i = 0; i < sz; ++i) f_a[i] = (ll) f_a[i] * f_b[i] % MOD;
+	ntt(f_a, sz, 1); return vector <int> (f_a, f_a + n + m - 1);
+}
+
+// G = primitive_root(MOD)
+int primitive_root (int p) {
+	vector <int> factor;
+	int tmp = p - 1;
+	for (int i = 2; i * i <= tmp; ++i) {
+		if (tmp % i == 0) {
+			factor.emplace_back(i);
+			while (tmp % i == 0) tmp /= i;
+		}
+	}
+	if (tmp != 1) factor.emplace_back(tmp);
+	for (int root = 1; ; ++root) {
+		bool flag = true;
+		for (int i = 0; i < (int) factor.size(); ++i) {
+			if (bigMod(root, (p - 1) / factor[i], p) == 1) {
+				flag = false; break;
+			}
+		}
+		if (flag) return root;
+	}
+}
 
 int n, k;
 int h[N];
-int inv[N];
-int fact[N];
-int ifact[N];
 
-int nCk(int n, int k) {
-	if (n < k) return 0;
-	return fact[n] * 1LL * ifact[k] % MOD * ifact[n - k] % MOD;
+vector<int> polyPower(vector<int> a, int e) {
+	vector<int> ret = {1};
+	while (e) {
+		if (e & 1) ret = multiply(ret, a);
+		a = multiply(a, a);
+		e /= 2;
+	}
+	return ret;
+}
+
+int numPower(int a, int e) {
+	int ret = 1;
+	while (e) {
+		if (e & 1) ret = ret * 1LL * a % MOD;
+		a = a * 1LL * a % MOD;
+		e /= 2;
+	}
+	return ret;
 }
 
 int main() {
@@ -34,31 +117,19 @@ int main() {
 	scanf("%d %d", &n, &k);
 	for (int i = 1; i <= n; i++) scanf("%d", &h[i]);
 
-	int diff = 0;
-	for (int i = 1; i <= n; i++) diff += h[i] != h[i % n + 1];
-	int p2diff = 1;
-	for (int i = 1; i <= diff; i++) p2diff = p2diff * 2LL % MOD;
+	int cnt[2] = {0, 0};
+	for (int i = 1; i <= n; i++) cnt[h[i] == h[i % n + 1]]++;
 
-	inv[1] = fact[0] = ifact[0] = 1;
-	for (int i = 2; i < N; i++) inv[i] = inv[MOD % i] * 1LL * (MOD - MOD / i) % MOD;
-	for (int i = 1; i < N; i++) fact[i] = fact[i - 1] * 1LL * i % MOD;
-	for (int i = 1; i < N; i++) ifact[i] = ifact[i - 1] * 1LL * inv[i] % MOD;
+	vector<int> v = polyPower({1, (k - 2) % MOD, 1}, cnt[0]);
 
-	ll ans = 0LL;
-	int zero_ways = 1;
-	int p2nonzero = p2diff;
-	for (int zero_cnt = 0; zero_cnt <= diff; zero_cnt++) {
-		int nonzero_cnt = diff - zero_cnt;
-		int nonzero_ways = ((ll)p2nonzero - (nonzero_cnt & 1 ? 0 : nCk(nonzero_cnt, nonzero_cnt / 2)) + MOD) * 1LL * inv[2] % MOD;
-		ans = (ans + zero_ways * 1LL * nCk(diff, zero_cnt) % MOD * nonzero_ways) % MOD;
-
-		zero_ways = zero_ways * 1LL * (k - 2) % MOD;
-		p2nonzero = p2nonzero * 1LL * inv[2] % MOD;
+	int ans = 0;
+	for (int i = cnt[0] + 1; i < v.size(); i++) {
+		ans = (ans + v[i]) % MOD;
 	}
 
-	for (int i = 1; i <= n - diff; i++) ans = ans * 1LL * k % MOD;
+	ans = ans * 1LL * numPower(k % MOD, cnt[1]) % MOD;
 
-	printf("%lld\n", ans);
+	printf("%d\n", ans);
 
 	return 0;
 }
