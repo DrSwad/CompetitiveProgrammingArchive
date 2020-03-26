f8062f104206874b51b2c1645021e64e7d59b21a
Nayeemul Islam Swad <drswad2013@gmail.com>
2020-03-26 20:37:00 +0600

    Add solution for problem `A - Divisibility Problem` Link: https://codeforces.com/contest/1328/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1328/submission/74399515

---
+++ codeforces/1328/A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+#define x first
+#define y second
+
+#ifdef LOCAL
+#include "/Users/swad/Desktop/CP/debug.h"
+#endif
+
+const int N = int(1e5) + 10;
+
+
+
+int main() {
+	#ifdef LOCAL
+	freopen("in", "r", stdin);
+	freopen("out", "w", stdout);
+	#endif
+
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int a, b;
+		scanf("%d %d", &a, &b);
+		if (a % b == 0) puts("0");
+		else printf("%d\n", b - a % b);
+	}
+
+	return 0;
+}
